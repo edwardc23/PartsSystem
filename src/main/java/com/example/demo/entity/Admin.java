09bb9a14a8ff0dac79f40017ca2baf47d26bcb9a
Edward Cochran <edwardcochran23@gmail.com>
2020-05-04 17:04:50 -0400

    Added Parts Entity and Admin Entity

---
+++ src/main/java/com/example/demo/entity/Admin.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="adminInfo")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private int id;
+
+    @Column(name="userName")
+    private String userName;
+
+    @Column(name="Password")
+    private String password;
+
+    public Admin(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                ", Username='" + userName + '\'' +
+                ", Password='" + password + '\'' +
+                '}';
+    }
+}
